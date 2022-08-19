cc665a1cd4778e0194d410c267a9f708ad974558
Keturahkalio <99884773+Keturahkalio@users.noreply.github.com>
2022-08-19 16:11:33 +0100

    Add files via upload

---
+++ EMPLOYEE TASK 2.sql
@@ -0,0 +1,20 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE employees(
+  employee_id INT PRIMARY KEY,
+  employee_start_date TIMESTAMP,
+  employee_birthday TIMESTAMP,
+  employee_first_name CHAR,
+  employee_middle_name CHAR,
+  employee_last_name CHAR,
+  employee_city CHAR,
+  employee_state CHAR,
+  employee_country CHAR,
+  employee_postcode VARCHAR);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
